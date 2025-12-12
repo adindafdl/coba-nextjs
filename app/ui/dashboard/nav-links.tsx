f190546341265fcd06cb20b50f02fc36ff5903c6
Adinda Putri Nur Fadila <adindafdl11@gmail.com>
2025-12-13 00:29:17 +0700

    'update'

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -22,7 +22,6 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
-
   return (
     <>
       {links.map((link) => {
@@ -41,5 +40,5 @@ export default function NavLinks() {
         );
       })}
     </>
-  );
+  ); 
 }
