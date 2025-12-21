e8056a74b9b29b2eca7513145e245075175a5b60
Adinda Putri Nur Fadila <adindafdl11@gmail.com>
2025-12-21 10:41:53 +0700

    add trash

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -16,8 +16,7 @@ export function CreateInvoice({ id }: { id: string }) {
 
 export function UpdateInvoice({ id }: { id: string }) {
   return (
-    <Link
-      href={`/dashboard/invoices/${id}/edit`}
+    <Link href={`/dashboard/invoices/${id}/edit`}
       className="rounded-md border p-2 hover:bg-gray-100"
     >
       <PencilIcon className="w-5" />
