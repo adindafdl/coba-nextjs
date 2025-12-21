aab4bbb34649bcdb7d1c065ca535a8a05daab41d
Adinda Putri Nur Fadila <adindafdl11@gmail.com>
2025-12-21 13:20:50 +0700

    fix problem

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,7 +9,6 @@ import { AuthError } from 'next-auth';
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
- 
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
